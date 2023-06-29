467f101b6b76900efbc04fd0e63b673f1f82626b
Destany <destany@zipcoders-mbp.mynetworksettings.com>
2023-06-28 20:32:16 -0400

    Alice and Bob Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        System.out.print("Please enter your name: ");
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+
+        if (a.equalsIgnoreCase("Alice")) {
+            System.out.println("Hello Alice");
+        } else if (a.equalsIgnoreCase("Bob")){
+            System.out.println("Hello Bob");
+
+        }
 
     }
 }
